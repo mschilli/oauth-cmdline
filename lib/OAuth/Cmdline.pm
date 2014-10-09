c4ff91705656940c331849c11d412fb8889f5a0a
mschilli <github@perlmeister.com>
2014-10-08 20:42:21 -0700

    Added access_type=offline ([rt.cpan.org #99372]) since Google apparently won't sent a refresh token in some cases if it's not set.

--- lib/OAuth/Cmdline.pm
+++ lib/OAuth/Cmdline.pm
@@ -24,11 +24,12 @@ has homedir => (
   is      => "ro",
   default => glob '~',
 );
-has login_uri => ( is => "rw" );
-has site      => ( is => "rw" );
-has scope     => ( is => "rw" );
-has token_uri => ( is => "rw" );
-has redir_uri => ( is => "rw" );
+has login_uri   => ( is => "rw" );
+has site        => ( is => "rw" );
+has scope       => ( is => "rw" );
+has token_uri   => ( is => "rw" );
+has redir_uri   => ( is => "rw" );
+has access_type => ( is => "rw" );
 
 ###########################################
 sub redirect_uri {
@@ -60,6 +61,8 @@ sub full_login_uri {
       response_type => "code",
       redirect_uri  => $self->redirect_uri(),
       scope         => $self->scope(),
+      ($self->access_type() ?
+          (access_type => $self->access_type()) : ()),
     );
 
     return $full_login_uri;
